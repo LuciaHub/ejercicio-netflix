9e02519b4a6826dc11399e8f6ae45b225fe72aac
LuciaHub <luciaprogramacion11@gmail.com>
2020-02-11 14:01:32 +0100

    hola

--- artifact/src/main/java/group/id/App.java
+++ artifact/src/main/java/group/id/App.java
@@ -1,12 +1,17 @@
 package group.id;
 
+import java.util.ArrayList;
+
+
+
 /**
  * Hello world!
  *
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+       Pelicula ABC = new Pelicula("abc", "abc", "abc", "abc","abc");
+       ABC.reproducir();
     }
 }
 
@@ -62,33 +67,42 @@ class Pelicula extends Contenido implements IReproducible {
         System.out.println("retrocediento");
     }
 
+}
 
-
-class Serie extends Contenido implements IReproducible {
-    public Serie(String titulo, String descripcion, String fecha, String director, String productora) {
-        super(titulo, descripcion, fecha, director, productora);
-    }
-
+class Capitulo implements IReproducible {
     public void reproducir() {
         System.out.println("reproducir");
     }
-	
-	public void adelantar() {
-		System.out.println("adelantando");
-	}
-    
+
     public void iniciar(){
         System.out.println("iniciando");
     }
-	public void pausar() {
+  
+    public void pausar() {
         System.out.println("pausando");
-    }	
-    
+    }
+
+    public void adelantar(){
+        System.out.println("adelantando");
+    }
     public void retroceder(){
         System.out.println("retrocediento");
     }
 
+}
+
+class Serie extends Contenido {
+    private ArrayList<Capitulo> capitulos = new ArrayList<Capitulo>();
+
+
+    public Serie(String titulo, String descripcion, String fecha, String director, String productora) {
+        super(titulo, descripcion, fecha, director, productora);
     }
+    public void add (Capitulo capitulos){
+        this.capitulos.add(capitulos);
+    }
+
+ 
     
 
     
